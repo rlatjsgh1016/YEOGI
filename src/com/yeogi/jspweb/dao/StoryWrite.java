4ed2410e2784920ad505e5e02ac8ecb9e0a18b5d
user <user@DESKTOP-PNJ5TCE>
2018-02-13 17:32:57 +0900

    dskim

---
+++ src/com/yeogi/jspweb/dao/StoryWrite.java
@@ -0,0 +1,16 @@
+package com.yeogi.jspweb.dao;
+
+import java.util.List;
+
+public interface StoryWrite {
+	void set(String str);
+	void set(int num);
+	void set(char yn);
+	
+	void setLogPost(String query);
+	void addLog
+	
+	
+	List<TourLog> getList();
+	
+}
