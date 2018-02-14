8c08acd4f0ce1ae92e058c9c72a0b8d6f72fb22e
user <user@DESKTOP-83S5L4S>
2018-02-14 17:14:24 +0900

    [planDao modified]

--- src/com/yeogi/jspweb/dao/PlanDao.java
+++ src/com/yeogi/jspweb/dao/PlanDao.java
@@ -1,29 +1,21 @@
+import java.util.Date;
 
 public interface PlanDao {
-	void set(String str);
-	void set(int num);
-	void set(String date)
+
+	void insert(String travelTitle, Date startDate, Date endDate, String theme, int companion)
 	
 	
-	//계획초기창
+
 	Tourplan getTourplan(String id);
 	
-	//타임테이블
-	PlanPost getPlanPost(String id);
+
+	List<Location> getLocation();
+	List<Location> getLocation(String query);
 	
-	//지출관련 메모
-	PlanPostSpend getPlanPostSpend(String id);
-	
-	//왜 ? 
-	Tour_Plan getTourPlan(String id);
-	
-	List<Loc> getLoc();
-	List<Loc> getLoc(String query);
-	
-	List<t_plan_mod_ahtu_list> getPlanFriend();
-	List<t_plan_post> getPlanPost(String id);
-	List<t_plan_loc_list> getLocList(int day);
-	List<t_Plan_Post_Spd_list> getPlanSpendList();
+	List<PlanModifyList> getModifyFriend();
+	List<Post> getPost(String id);
+	List<LocationList> getLocationList(int day);
+	List<PostSpendList> getSpendList();
 	
 	
 	
