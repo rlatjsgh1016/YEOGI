d3b9aeb77b35f473a3afdc61b05bf01777d647a5
DSKIM <DSKIM@DESKTOP-OTGCDQ4>
2018-02-19 00:35:18 +0900

    dskim

--- src/com/yeogi/jspweb/dao/StoryWrite.java
+++ src/com/yeogi/jspweb/dao/StoryWrite.java
@@ -3,14 +3,61 @@ package com.yeogi.jspweb.dao;
 import java.util.List;
 
 public interface StoryWrite {
-	void set(String str);
-	void set(int num);
-	void set(char yn);
+	List<Friend> getList(String mId);
+	int insert(Friend frd);
+	int update(Friend frd);
 	
-	void setLogPost(String query);
-	void addLog
+	List<MLocScrap> getList(String mId);
+	int insert(MLocScrap mls);
+	int update(MLocScrap mls);
 	
+	List<Loc> getList();
+	int insert(Loc loc);
+	int update(Loc loc);
 	
-	List<TourLog> getList();
+	List<MyLoc> getList();
+	int insert(MyLoc mLoc);
+	int update(MyLoc mLoc);
 	
+	List<TLogLocList> getList(TourLog tourLog);
+	int insert(TLocLocList tlll);
+	int update(TLocLocList tlll);
+	
+	List<TLogModAuthList> getList(TourLog tourLog);
+	int insert(TLogModAuthList tlmal);
+	int update(TLogModAuthList tlmal);
+	
+	List<TLogNationList> getList(TourLog tourLog);
+	int insert(TLogNationList tlnl);
+	int update(TLogNationList tlnl);
+	
+	List<DTLogLocList> getList(Day day);
+	int insert(DTLogLocList dtlll);
+	int update(DTLogLocList dtlll);
+		
+	List<TLogPost> getList(TourLog tourLog, TLogLocList tLogLocList);
+	int insert(TLogPost tlp);
+	int update(TLogPost tlp);
+	
+	List<Tag> getList(String tLogPostId);
+	int insert(Tag tag);
+	int update(Tag tag);
+	
+	List<TLogPostImgList> getList(String tLogPostId);
+	int insert(TLogPostImgList tlpil);
+	int update(TLogPostImgList tlpil);
+	
+	List<TLogPostSpdList> getList(String tLogPostId);
+	int insert(TLogPostSpdList tlpsl);
+	int update(TLogPostSpdList tlpsl);
+	
+	List<Trans> getList();
+	List<TTheme> getList();
+	List<Day> getList();
+	List<Nation> getList();
+	
+	Member get(String id);
+	TourLog get(String id);
+	
+	int delete(String id);
 }
