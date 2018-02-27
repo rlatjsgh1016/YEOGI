16d7724f7329d09071768af3506b4bbe08d7a998
sist <sist@DESKTOP-KJ35G0A>
2018-02-27 17:16:41 +0900

    commit

---
+++ src/com/yeogi/jspweb/dao/Commu_PostDao.java
@@ -0,0 +1,18 @@
+package com.yeogi.jspweb.dao;
+
+import java.util.List;
+
+import com.yeogi.jspweb.entity.Commu_Post;
+import com.yeogi.jspweb.entity.Commu_Post_Img;
+
+	public interface Commu_PostDao {
+		
+		List<Commu_Post_Img> getList();
+
+		public int delete(String id );
+		public int insert(Commu_Post cp);
+		public int update(Commu_Post cp);
+
+		
+		
+}
