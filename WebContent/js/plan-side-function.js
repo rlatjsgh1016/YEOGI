b714387b0783a09a347961d0b869dd7ac73b067d
min <min@min-PC>
2018-03-09 01:20:55 +0900

    sist

--- WebContent/js/plan-side-function.js
+++ WebContent/js/plan-side-function.js
@@ -6,8 +6,41 @@ window.addEventListener("load", function () {
     var right = document.querySelector("#add-day .right-direct");
     var allminus = document.querySelector("#add-day .all-minus");
     var count = parseInt(now.textContent);
-    
     /*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
+    var memoButton = document.getElementsByClassName("btn-memo");
+    var modal = document.getElementsByClassName("modal");
+    var close = document.getElementsByClassName("close");
+    var save = document.querySelector(".save");
+    
+    /*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 메모창  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
+    
+   /* memoButton.onclick = function(){
+    	modal.style.display = "block";
+    };*/
+    
+   
+    for(var i=0; i<memoButton.length; i++){
+    	memoButton[i].onclick = function(){
+			for(var i=0; i<modal.length; i++){
+				modal[i].style.display = "block";
+			}
+		};
+	}
+    
+    
+    for(var i=0; i<close.length; i++){
+    	close[i].onclick = function(){
+			for(var i=0; i<modal.length; i++)
+				modal[i].style.display = "none";
+			
+		};
+	}
+    
+    
+    
+    
+    /*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 메모창  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
+    
     
     plus.onclick = function () {
     	if(count<60)
@@ -30,7 +63,7 @@ window.addEventListener("load", function () {
 		now.textContent = count;
     };
     
-    /*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 메모창  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
+    
     
     	
     
