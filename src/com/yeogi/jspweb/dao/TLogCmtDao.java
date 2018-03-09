cce17ed377024689cf1063fd7c7023e9f1b8b23e
sist80 <sist80@DESKTOP-BEITEAV>
2018-03-09 12:15:18 +0900

    sg

--- src/com/yeogi/jspweb/dao/TLogCmtDao.java
+++ src/com/yeogi/jspweb/dao/TLogCmtDao.java
@@ -1,7 +1,18 @@
 package com.yeogi.jspweb.dao;
 
+import java.util.List;
+
+import com.yeogi.jspweb.entity.Member;
+import com.yeogi.jspweb.entity.TLogCmt;
+
 public interface TLogCmtDao {
 	
-	
+	List<TLogCmt> getList(TLogCmt tLogCmt);
+
+	int insert(Member id);
+
+	int update(Member id);
+
+	int delete(String id);
 
 }
