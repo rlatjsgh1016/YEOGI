7ca4881368cb397427ae25377a989aff82d554dd
SIST <SIST@DESKTOP-NHJHCG8>
2018-03-12 19:53:17 +0900

    GO

--- WebContent/js/plan-side-function.js
+++ WebContent/js/plan-side-function.js
@@ -1,12 +1,15 @@
 window.addEventListener("load", function () {
-
+	/*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 일정추가  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
     var plus = document.querySelector("#add-day .plus");
     var now = document.querySelector("#add-day .now span");
     var left = document.querySelector("#add-day .left-direct");
     var right = document.querySelector("#add-day .right-direct");
     var allminus = document.querySelector("#add-day .all-minus");
+    var minus = document.querySelector("#add-day .minus");
     var count = parseInt(now.textContent);
-    /*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
+    var num = 1;
+    var lastday =1;
+    /*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 메모버튼 업 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
     var memoButton = document.getElementsByClassName("btn-memo");
     var modal = document.getElementsByClassName("modal");
     var close = document.getElementsByClassName("close");
@@ -22,18 +25,67 @@ window.addEventListener("load", function () {
     
     var clone = verySad.cloneNode(true);
     var count = 0;
-    /*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
+    /*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ친구 추가ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
+    var size = document.getElementsByName("checked-friend").length;
+	var addFriend = document.querySelector(".add-friend");
+	
+	
+	var checkedFriend = document.getElementsByName("checked-friend");
+	var ple = "";
+	var msg = "님께 함께 계획짜기를 요청했습니다  :)\n";
+	var waitBox = document.querySelector(".wait-box");
+	
+	var addFriendButton = document.querySelector("#add-friend");
+	var modalFriend = document.querySelector(".modal-friend");
+	var close1 = document.querySelector(".close1");
+	/*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ친구 추가ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
+	addFriendButton.onclick = function(){
+		modalFriend.style.display = "block";
+	}
+	
+	close1.onclick = function(){
+		modalFriend.style.display = "none";
+	}
+	
+	    
+	
+	
+	addFriend.onclick = function() {
+		for (var i = 0; i < checkedFriend.length; i++) {
+			
+			if (checkedFriend[i].checked) {
+				ple += checkedFriend[i].value + " ";
+				
+				if(waitBox.innerHTML.indexOf(checkedFriend[i].value)==-1)
+				waitBox.innerHTML+=checkedFriend[i].value+"\n님<br/><br/>";
+				
+			}
+			
+		}
+		alert(ple + msg);
+		ple="";
+	
+	}
     
     
     
     
     
     
+     /*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 비용 + - 버튼 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
     
-    
-    
-    
-     /*ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ + - 버튼 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ*/
     costPlus.onclick = function(e){
     	clone = verySad.cloneNode(true);
     	addedFormDiv.appendChild(clone);
@@ -43,13 +95,11 @@ window.addEventListener("load", function () {
     costMinus.onclick = function(e){
     	if(count==1){
     	addedFormDiv.removeChild(clone);
-    	count--;
+    	count = 1;
     	}else{
     		addedFormDiv.removeChild(clone);
     		count = 1;
     	}
-    		
-    	
     }
     
    
@@ -75,8 +125,9 @@ window.addEventListener("load", function () {
     
     for(var i=0; i<close.length; i++){
     	close[i].onclick = function(){
-			for(var i=0; i<modal.length; i++)
+			for(var i=0; i<modal.length; i++){
 				modal[i].style.display = "none";
+			}
 			
 		};
 	};
@@ -88,22 +139,41 @@ window.addEventListener("load", function () {
     
     
     plus.onclick = function () {
-    	if(count<60)
-        now.textContent = ++count;
+    	if(num<60){
+    		now.textContent = ++num;
+    		lastday++;
+    	}
+    	
         
     };
     
     left.onclick = function () {
-    	if(count >1)
-    		now.textContent = --count;
+    	if(num >1){
+    		now.textContent = --num;
+    	}else{
+    		alert("첫 일정입니다. 즐거운 여행되세요 :)");
+    	}
     	
     };
     
     right.onclick = function () {
-    		now.textContent = ++count;
+    		if(lastday == num){
+    			alert("마지막 일정입니다. 일정을 더 추가해주세요 :)");
+    		}
+    		if(lastday != num){
+    			now.textContent = ++num;
+    		}
     };
     
+    minus.onclick = function(){
+    	//delete num의 현재 데이터
+    	//show -1 day의 데이터
+    }
+    
+    
     allminus.onclick = function () {
+    	//delete all 데이터
+    	//show new 1dat 데이터 (빈칸)
     	count=1;
 		now.textContent = count;
     };
@@ -111,9 +181,7 @@ window.addEventListener("load", function () {
     
     
     	
-    
-    
-   
+
     
     
     
--- src/com/yeogi/jspweb/controller/main/member/plan/schedule/ScheduleController.java
+++ src/com/yeogi/jspweb/controller/main/member/plan/schedule/ScheduleController.java
@@ -15,6 +15,7 @@ import com.yeogi.jspweb.dao.TPlanPostSpdDao;
 import com.yeogi.jspweb.dao.jdbc.JdbcTPlanPostDao;
 import com.yeogi.jspweb.dao.jdbc.JdbcTPlanPostSpdDao;
 import com.yeogi.jspweb.entity.TPlanPost;
+import com.yeogi.jspweb.entity.TPlanPostSpd;
 import com.yeogi.jspweb.entity.TPlanPostSpdView;
 
 @WebServlet("/main/member/plan/schedule/schedule")
@@ -33,4 +34,54 @@ public class ScheduleController extends HttpServlet {
 		dispatcher.forward(request, response);
 		
 	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+	    response.setContentType("text/html; charset=UTF-8");
+	    request.setCharacterEncoding("UTF-8");
+	    
+	    TPlanPostSpd tplanpostspd = new TPlanPostSpd();
+	    
+	    tplanpostspd.setId("2018031200011");
+	    tplanpostspd.setTPlanId("2018031200011");
+	    tplanpostspd.setTPlanLocId("2018031200011");
+	    tplanpostspd.setAmount(request.getParameter("input-cost"));
+	    tplanpostspd.setContent(request.getParameter("content"));
+	    tplanpostspd.setType("명소");
+	    tplanpostspd.setUnit("달러");
+	    
+	    TPlanPostSpdDao tPlanPostSpdDao = new JdbcTPlanPostSpdDao();
+	    tPlanPostSpdDao.insert(tplanpostspd);
+	    
+	    
+	    TPlanPost tplanpost = new TPlanPost();
+	    
+	    tplanpost.setMemoTitle(request.getParameter("memo-title"));
+	    tplanpost.setMemoContent(request.getParameter("memo-content"));
+	    tplanpost.setTourDateTime("23:00");
+	    tplanpost.setTPlanId("2018031200011");
+	    tplanpost.setTPlanLocId("201803120011");
+	    
+	    TPlanPostDao tPlanPostDao = new JdbcTPlanPostDao();
+	    tPlanPostDao.insert(tplanpost);
+	    
+	    
+	    response.sendRedirect("/WEB-INF/views/main/member/plan/schedule/schedule.jsp");
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
