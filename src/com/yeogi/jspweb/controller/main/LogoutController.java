c80d32bd3e85bfc00ac1bea64e8e743fce597399
ss_2F <ss_2F@DESKTOP-NB1UAMK>
2018-03-14 19:15:53 +0900

    sh

---
+++ src/com/yeogi/jspweb/controller/main/LogoutController.java
@@ -0,0 +1,29 @@
+package com.yeogi.jspweb.controller.main;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tiles.TilesContainer;
+import org.apache.tiles.access.TilesAccess;
+import org.apache.tiles.request.ApplicationContext;
+import org.apache.tiles.request.servlet.ServletRequest;
+import org.apache.tiles.request.servlet.ServletUtil;
+
+@WebServlet("/main/logout")
+public class LogoutController extends HttpServlet {
+
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getSession().invalidate();
+		response.sendRedirect("main");
+
+	
+	}
+}
